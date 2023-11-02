a20b53fec679949f6858a78bf6de112d6ecc2e93
katekoso <116917623+katekoso@users.noreply.github.com>
2023-11-02 16:18:33 +0300

    added Card in App.js and some variables

--- languages/src/App.js
+++ languages/src/App.js
@@ -1,11 +1,20 @@
 import './assets/styles/App.css';
 import List from './assets/components/List/List';
+import Card from './assets/components/Card/Card';
+const data = require('./assets/components/data.json');
 
 function App() {
   return (
+    <>
+    {
+      data.map((word) =>
+          <Card word={word.word} translation={word.translation} transcription={word.transcription} key={word.word}/>
+      )
+    }
     <div className="listContainer">
     <List></List>
     </div>
+    </>
   );
 }
 
