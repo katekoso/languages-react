1a7dafcea449f0a0e84fc91b7c3278ea4bc23e40
katekoso <116917623+katekoso@users.noreply.github.com>
2023-11-08 22:51:36 +0300

    some polishing

--- languages/src/assets/components/Slider/Slider.jsx
+++ languages/src/assets/components/Slider/Slider.jsx
@@ -7,56 +7,45 @@ import SliderButton from './SliderButton';
 function Slider(props) {
     const { words, firstWordIndex } = props;
     const [selectedIndex, setSelectedIndex] = useState(firstWordIndex);
-    //const buttonRef = useRef(null);
 
-    const changeIndexPrevious = () => {
+    const showPrevious = () => {
         if(selectedIndex !== 0){
             setSelectedIndex(selectedIndex - 1)
         }
         else if (selectedIndex === 0){
             setSelectedIndex(words.length - 1)
         }
-    }
-
-    const changeIndexNext = () => {
+      }
+      
+      const showNext = () => {
         if(selectedIndex !== words.length - 1){
             setSelectedIndex(selectedIndex + 1)
         } 
         else if (selectedIndex === words.length - 1){
             setSelectedIndex(0)
         }
-    }
-
-    const showPrevious = () => {
-        changeIndexPrevious();
-      }
-      
-      const showNext = () => {
-        changeIndexNext();
       }
 
     return (
         <div className={styles.slider}>
             <div className={styles.slider__main}>
-        <SliderButton direction={"prev"} moveSlide={showPrevious}/>
-        <SwitchTransition mode="out-in">
-                <CSSTransition 
-                key={selectedIndex}
-            timeout={500}
-            classNames={{
-                enterActive: styles.myClassEnterActive,
-                enterDone: styles.myClassEnterDone,
-                exitActive: styles.myClassExitActive,
-                exitDone: styles.myClassExitDone
-            }}
-            >
-           <Card english={words[selectedIndex].english} russian={words[selectedIndex].russian} transcription={words[selectedIndex].transcription} key={words[selectedIndex].id}/>
-           </CSSTransition>
-      </SwitchTransition>
-        <SliderButton direction={"next"} moveSlide={showNext}/>
+                <SliderButton direction={"prev"} moveSlide={showPrevious}/>
+                <SwitchTransition mode="out-in">
+                    <CSSTransition 
+                        key={selectedIndex}
+                        timeout={500}
+                        classNames={{
+                            enterActive: styles.myClassEnterActive,
+                            exitActive: styles.myClassExitActive
+                        }}
+                    >
+                        <Card english={words[selectedIndex].english} russian={words[selectedIndex].russian} transcription={words[selectedIndex].transcription} key={words[selectedIndex].id}/>
+                    </CSSTransition>
+                </SwitchTransition>
+                <SliderButton direction={"next"} moveSlide={showNext}/>
+            </div>
+            <div className={styles.slider__count}>{selectedIndex + 1}/{words.length}</div>
         </div>
-        <div className={styles.slider__count}>{selectedIndex + 1}/{words.length}</div>
-      </div>
     );
 }
 
