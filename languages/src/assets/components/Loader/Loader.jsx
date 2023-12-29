53b94c2bfe67b94d7edfb7392998849e69cf38b4
katekoso <116917623+katekoso@users.noreply.github.com>
2023-12-29 13:21:29 +0300

    changed class to className in Loader

--- languages/src/assets/components/Loader/Loader.jsx
+++ languages/src/assets/components/Loader/Loader.jsx
@@ -2,8 +2,8 @@ import styles from './loader.module.scss';
 
 function Loader() {
     return (
-        <div class={styles.cssloadContainer}>
-            <div class={styles.cssloadDoubleTorus}></div>
+        <div className={styles.cssloadContainer}>
+            <div className={styles.cssloadDoubleTorus}></div>
         </div>
     );
 }
